eef0c40186c4abedc67d5cd42fbf5a7d763d382d
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-07-28 08:27:15 -0300

    Implement Vectorized regularized Logistic Regression on exercise 3.

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,6 +36,32 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+%h_theta = g(theta^t * x) --> G = 1/(1+e^-(z))
+%J_logistic_reg = sum i from 1:m  - (y)(log(h_theta(x))) - (1-y)(log(1-h_theta(x))) + (lambda/2m)* sum j from 1:n theta_j^2
+
+%X (m,n+1)
+%theta (n+1,1)
+%y(m,1)
+
+h_theta_lin = X*theta;
+%h_theta_lin (m,1)
+
+%prob of each h_theta is y
+%h_theta_log = 1/(1+e^(-h_theta_lin));
+h_theta_log = sigmoid(h_theta_lin);
+%h_theta_log (m,1)
+
+J = -(1/m)* (log(h_theta_log)' * y + log(1-h_theta_log)' * (1-y)) + (lambda/(2*m))*sum(theta([2:size(theta,1),:]).^2);
+
+
+%diff_matrix (m,1)
+diff_matrix = h_theta_log - y;
+
+%Derivative of J_theta w.r.t = (1/m) * sum i=1:m (h_theta_log - y)*x^i + (lambda/m)*theta_j j from initial_theta+1:n
+grad = zeros(size(theta));
+grad = (1/m) * X' * diff_matrix + (lambda/m)*theta;
+grad(1,:) = (1/m) * X(:,1)' * diff_matrix;  %overwrites first element
+
 
 
 
