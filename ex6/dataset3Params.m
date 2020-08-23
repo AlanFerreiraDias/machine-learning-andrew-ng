6fc1f7473c884b953bd2bfbda5dcee37cd5478c6
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-23 15:13:23 -0300

    Implement function to choose the best SVM params.

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,37 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+x1 = Xval(:,1);
+x2 = Xval(:,2);
 
+indexC = 1;
+indexSig = 1;
+indexAux = 1;
 
+c = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigAux = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+error_matrix = zeros(size(c,2),3);
 
+for c = c
+    for sig = sigAux
+        C = c;
+        sigma = sig;
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+        predictions = svmPredict(model, Xval);
+        error_matrix(indexAux,:) = [c; sig; mean(double(predictions ~= yval))];
+        indexSig = indexSig + 1;
+        indexAux = indexAux + 1;
+    endfor
+    indexSig = 1;
+    indexC = indexC + 1;
+endfor 
 
+[min_error, min_index] = min(error_matrix(:,3))
+chosenParams = error_matrix(min_index,:)
+
+C = chosenParams(1,1);
+sigma = chosenParams(1,2);
 
 % =========================================================================
 
