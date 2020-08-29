67c6fdce84eb4c5bc06dba84f869342293362943
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-29 16:19:57 -0300

    Create function to recover compressed data

--- ex7/recoverData.m
+++ ex7/recoverData.m
@@ -21,7 +21,8 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+%%Need to think a bit more about the dimentions
+X_rec = Z * U(:,1:K)';
 
 % =============================================================
 
